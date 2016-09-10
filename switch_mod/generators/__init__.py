431acdc32d0394452ee136c354848dc01d421075
josiah.johnston <josiah.johnston@gmail.com>
2016-09-09 16:46:51 -1000

    Make a new subdirectory (package) for customizing generators, and move hydro_simple into it.

---
+++ switch_mod/generators/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2015 The Switch Authors. All rights reserved.
+# Licensed under the Apache License, Version 2, which is in the LICENSE file.
+
+"""
+
+This package includes specific types of generators that require extra model
+components. 
+
+"""
