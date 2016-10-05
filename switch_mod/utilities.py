0b309d619c2fdaaa9a5d69f84dddf70539570327
bmaluenda <bmaluendap@gmail.com>
2016-10-05 16:34:20 -0300

    Adding a command line option to allow output to be printed in a sorted manner.
    Default is false, no that speed is always prioritized.

--- switch_mod/utilities.py
+++ switch_mod/utilities.py
@@ -127,8 +127,7 @@ def load_inputs(model, inputs_dir=None, attachDataPortal=True):
     return instance
 
 
-def save_inputs_as_dat(model, instance, save_path="inputs/complete_inputs.dat",
-                       exclude=[], deterministic_order=False):
+def save_inputs_as_dat(model, instance, save_path="inputs/complete_inputs.dat", exclude=[], sorted_output=False):
     """
     Save input data to a .dat file for use with PySP or other command line
     tools that have not been fully integrated with DataPortal.
@@ -174,7 +173,7 @@ def save_inputs_as_dat(model, instance, save_path="inputs/complete_inputs.dat",
                     else:
                         f.write("\n")
                         for key,value in (sorted(component_data.iteritems()) 
-                                          if deterministic_order 
+                                          if sorted_output
                                           else component_data.iteritems()):
                             f.write(" " + 
                                     ' '.join(map(str, key)) + " " +
