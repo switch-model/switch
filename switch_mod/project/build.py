3b53e1fa39fb329da87341bd0aca7f1b48efcc98
bmaluenda <bmaluendap@gmail.com>
2016-11-03 15:18:07 -0300

    Switched the minimum capacity build constraints to the build module. The user may turn this constraints on or off by inputting or not the g_min_build_capacity parameter for each gentech.

--- switch_mod/project/build.py
+++ switch_mod/project/build.py
@@ -117,6 +117,23 @@ def define_components(mod):
     be operated. It can either indicate retirement or the end of the
     simulation. This is derived from g_max_age.
 
+    NEW_PROJ_WITH_MIN_BUILD_YEARS is the subset of NEW_PROJ_BUILDYEARS for
+    which minimum capacity build-out constraints will be enforced.
+
+    ProjCommitToMinBuild[proj, build_year] is a binary variable that indicates
+    whether a project will build capacity in a period or not. If the model is
+    committing to building capacity, then the minimum must be enforced.
+
+    Enforce_Min_Build_Lower[proj, build_year]  and
+    Enforce_Min_Build_Upper[proj, build_year] are a pair of constraints that
+    force project build-outs to meet the minimum build requirements for
+    generation technologies that have those requirements. They force BuildProj
+    to be 0 when ProjCommitToMinBuild is 0, and to be greater than
+    g_min_build_capacity when ProjCommitToMinBuild is 1. In the latter case,
+    the upper constraint should be non-binding. The total demand over all
+    periods and load zones is used as  a limit, as in the extreme case where
+    all energy should have to be served in one hour.
+
     --- OPERATIONS ---
 
     PROJECT_BUILDS_OPERATIONAL_PERIODS[proj, build_year] is an indexed
@@ -353,6 +370,29 @@ def define_components(mod):
         mod.PROJECTS_CAP_LIMITED, mod.PERIODS,
         rule=lambda m, proj, p: (
             m.proj_capacity_limit_mw[proj] >= m.ProjCapacity[proj, p]))
+
+    # The following components enforce minimum capacity build-outs.
+    # Note that this adds binary variables to the model.
+    mod.NEW_PROJ_WITH_MIN_BUILD_YEARS = Set(
+        initialize=mod.NEW_PROJ_BUILDYEARS,
+        filter=lambda m, pr, p: (
+            m.g_min_build_capacity[m.proj_gen_tech[pr]] > 0))
+    mod.ProjCommitToMinBuild = Var(
+        mod.NEW_PROJ_WITH_MIN_BUILD_YEARS, within=Binary)
+    mod.Enforce_Min_Build_Lower = Constraint(
+        mod.NEW_PROJ_WITH_MIN_BUILD_YEARS,
+        rule=lambda m, proj, p: (
+            m.ProjCommitToMinBuild[proj, p] *
+            m.g_min_build_capacity[m.proj_gen_tech[proj]] <=
+            m.BuildProj[proj, p]))
+    mod.Enforce_Min_Build_Upper = Constraint(
+        mod.NEW_PROJ_WITH_MIN_BUILD_YEARS,
+        rule=lambda m, proj, p: (
+            m.BuildProj[proj, p] <=
+            m.ProjCommitToMinBuild[proj, p] *
+            sum(m.lz_demand_mw[lz, tp] * m.tp_weight[tp]
+                for lz in m.LOAD_ZONES
+                for tp in m.TIMEPOINTS)))
 
     # Costs
     mod.proj_connect_cost_per_mw = Param(mod.PROJECTS, within=NonNegativeReals)
