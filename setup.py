0b08e9a02d32afcbae509aeabd58fd4ef3902479
Matthias Fripp <mfripp@hawaii.edu>
2017-01-24 15:50:12 -1000

    use setup.py from standardize_setup branch

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 """Setup script for SWITCH. 
-Use "python setup.py install" to install a copy in the site packages directory.
-Use "python setup.py develop" to activate SWITCH while preserving its current location.
 
+Use "pip install --upgrade ." to install a copy in the site packages directory.
+
+Use "pip install --upgrade --editable ." to install SWITCH to be run from its 
+current location.
+
+Optional dependencies can be added during the initial install or later by 
+running a command like this: 
+pip install --upgrade --editable .[advanced,database_access]
+
+Use "pip uninstall switch" to uninstall switch from your system.
 """
 
 import os
@@ -10,73 +18,48 @@ from setuptools import setup
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-required = [
-    "Pyomo"
-]
-
-# use 'python setup.py develop --user' to install switch
-# then use 'pip install --user switch[run_tests,demand_response]' to install optional dependencies
-extras = {
-    'run_tests': ['testfixtures'],
-    'demand_response': ['numpy', 'scipy', 'rpy2']
-}
-
-packages = ['switch_mod']
-
-entry_points = """
-    [console_scripts]
-    switch=switch_mod.main:main
-"""
-
-from distutils.cmd import Command
-from distutils.core import setup
-
-
-# based on https://justin.abrah.ms/python/setuppy_distutils_testing.html
-class RunTests(Command):
-    user_options = []
-    def initialize_options(self):
-        pass
-    def finalize_options(self):
-        pass
-    def run(self):
-        import sys, subprocess
-        raise SystemExit(subprocess.call([sys.executable, 'run_tests.py']))
-
 setup(
     name='SWITCH',
-    version='2.0b0',
+    version='2.0.0b0',
     maintainer='Matthias Fripp',
     maintainer_email='mfripp@hawaii.edu',
     url='http://switch-model.org',
-    license='Apache',
+    license='Apache v2',
     platforms=["any"],
     description='SWITCH Power System Planning Model',
     long_description=read('README'),
     classifiers=[
     'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Education',
     'Intended Audience :: End Users/Desktop',
     'Intended Audience :: Science/Research',
     'License :: OSI Approved :: Apache Software License',
     'Natural Language :: English',
     'Operating System :: Microsoft :: Windows',
+    'Operating System :: MacOS :: MacOS X',
     'Operating System :: Unix',
     'Programming Language :: Python',
     'Programming Language :: Unix Shell',
     'Topic :: Scientific/Engineering',
     'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    packages=packages,
+    packages=['switch_mod'],
     keywords=[
         'renewable', 'power', 'energy', 'electricity', 
         'production cost', 'capacity expansion', 
         'planning', 'optimization'
     ],
-    install_requires=required,
-    tests_require=extras['run_tests'],
-    extras_require=extras,
-    entry_points=entry_points,
-    cmdclass={
-        'test': RunTests
-    }
+    install_requires=[
+        'Pyomo>=4.4.1', # We need a version that works with glpk 4.60+
+        'testfixtures'  # used for standard tests
+    ],
+    extras_require={
+        # packages used for advanced demand response and progressive hedging
+        'advanced': ['numpy', 'scipy', 'rpy2', 'sympy'],
+        'database_access': ['psycopg2']
+    },
+    entry_points={
+        'console_scripts': ['switch = switch_mod.main:main']
+    },
 )
