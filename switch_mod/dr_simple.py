24f1ae52f9fad9a5dd59c1237aa40cc30ed71259
josiah.johnston <josiah.johnston@gmail.com>
2017-02-22 11:04:14 -0800

    Proposing changes. Renamed DR components to focus on DR Shift Services and avoid confusion with DR Shedding Services (aka voluntary load curtailment) which is not modeled here. Simplify formulation by including bounds in parameters and variables.

--- switch_mod/dr_simple.py
+++ switch_mod/dr_simple.py
@@ -2,10 +2,11 @@
 # Licensed under the Apache License, Version 2, which is in the LICENSE file.
 
 """
-Defines a simple DR scheme for the SWITCH-Pyomo model. Load in a certain
-load zone may be shifted between timepoints belonging to the same 
-timeseries at no cost, which allows assessing the potential value of
-demand shifting.
+Defines a simple Demand Response Shift Service for the SWITCH-Pyomo model.
+Load in a certain load zone may be shifted between timepoints belonging to the
+same timeseries at no cost, which allows assessing the potential value of
+demand shifting. This does not include a Shed Service (curtailment of load), 
+nor a Shimmy Service (fast dispatch for load following or regulation).
 
 """
 
@@ -17,68 +18,47 @@ def define_components(mod):
     
     """
     Adds components to a Pyomo abstract model object to describe a demand
-    response scheme on load zones.
+    response shift service.
 
-    dr_max_curtail_in_MW[(lz in LOAD_ZONES, t in TIMEPOINTS)] is a parameter
-    that describe the maximum demand curtailment in MW allowed in a load
-    zone at a specific timepoint. Its default value is 0.
+    dr_shift_down_limit_MW[(lz in LOAD_ZONES, t in TIMEPOINTS)] is a parameter
+    that describes the maximum reduction in demand for load-shifting demand
+    response (in MW) that is allowed in a load zone at a specific timepoint.
+    Its default value is 0, and it may not exceed the load.
 
-    dr_max_increment_in_MW[lz, t] is a parameter that describes the maximum
-    demand increment in MW allowed in a load zone at a specific timepoint.
-    Its default value is infinity.
+    dr_shift_up_limit_MW[lz, t] is a parameter that describes the maximum
+    increase in demand for load-shifting demand response (in MW) that is
+    allowed in a load zone at a specific timepoint. Its default value is
+    infinity.
 
     DemandResponse[lz,t] is a decision variable describing how much load
     in MW is reduced (if its value is negative) or increased (if
-    its value is positive).
+    its value is positive). This variable is bounded by dr_shift_down_limit_MW
+    and dr_shift_up_limit_MW.
     
-    DR_Max_Curtailment[lz,t] is a constraint that describes the maximum
-    curtailment allowed in MW for a load zone at a specific timepoint.
-
-    DR_Curtail_Allowed[lz,t] is a constraint that prevents curtailment
-    to be more than the local demand at each timepoint.
-
-    DR_Max_Increment[lz,t] is a constraint that describes the maximum
-    increment of demand allowed in MW for a load zone at a specific timepoint.
-
-    DR_Net_Zero[lz,ts in TIMESERIES] is a constraint that forces all the
+    DR_Shift_Net_Zero[lz,ts in TIMESERIES] is a constraint that forces all the
     changes in the demand to balance out over the course of each timeseries.
-
-    Notes:
-
-    DR_Max_Curtailment and DR_Curtail_Allowed can be merged in only one
-    equation, picking the minimum value between min(dr_max_curtail_in_MW,
-    lz_demand_mw).
     
     """
     
-    mod.dr_max_curtail_in_MW = Param(
+    mod.dr_shift_down_limit_MW = Param(
         mod.LOAD_ZONES, mod.TIMEPOINTS,
         default= 0.0,
-        within=NonNegativeReals)
-    mod.dr_max_increment_in_MW = Param(
+        within=NonNegativeReals,
+        validate=lambda m, value, z, t: value <= m.lz_demand_mw[z, t])
+    mod.dr_shift_up_limit_MW = Param(
         mod.LOAD_ZONES, mod.TIMEPOINTS,
         default= float('inf'),
         within=NonNegativeReals)
     mod.DemandResponse = Var(
         mod.LOAD_ZONES, mod.TIMEPOINTS,
-        within=Reals)
+        within=Reals,
+        bounds=lambda m, z, t: 
+        (
+            (-1.0) * m.dr_shift_down_limit_MW[z,t],
+            m.dr_shift_up_limit_MW[z,t]
+        ))
 
-    mod.DR_Max_Curtailment = Constraint(
-        mod.LOAD_ZONES, mod.TIMEPOINTS,
-        rule=lambda m, z, t:
-        m.DemandResponse[z, t] >= (-1.0) * m.dr_max_curtail_in_MW[z,t])
-
-    mod.DR_Curtail_Allowed = Constraint(
-        mod.LOAD_ZONES, mod.TIMEPOINTS,
-        rule=lambda m, z, t:
-        m.DemandResponse[z, t] >= (-1.0) * m.lz_demand_mw[z,t])
-
-    mod.DR_Max_Increment = Constraint(
-        mod.LOAD_ZONES, mod.TIMEPOINTS,
-        rule=lambda m, z, t:
-        m.DemandResponse[z, t] <= m.dr_max_increment_in_MW[z,t])
-
-    mod.DR_Net_Zero = Constraint(
+    mod.DR_Shift_Net_Zero = Constraint(
         mod.LOAD_ZONES, mod.TIMESERIES,
         rule=lambda m, z, ts:
         sum(m.DemandResponse[z, t] for t in m.TS_TPS[ts]) == 0.0)
@@ -92,7 +72,7 @@ def load_inputs(mod, switch_data, inputs_dir):
     Import demand response-specific data from an input directory.
 
     dr_data.tab
-        LOAD_ZONE, TIMEPOINT, dr_max_curtail_in_MW, dr_max_increment_in_MW
+        LOAD_ZONE, TIMEPOINT, dr_shift_down_limit_MW, dr_shift_up_limit_MW
 
     """
 
@@ -100,4 +80,4 @@ def load_inputs(mod, switch_data, inputs_dir):
         optional=True,
         filename=os.path.join(inputs_dir, 'dr_data.tab'),
         autoselect=True,
-        param=(mod.dr_max_curtail_in_MW, mod.dr_max_increment_in_MW))
+        param=(mod.dr_shift_down_limit_MW, mod.dr_shift_up_limit_MW))
