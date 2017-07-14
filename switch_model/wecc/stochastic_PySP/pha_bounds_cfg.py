70da3a28224f7197d22ba2882d8e6bdf266d5ed2
Patricia Hidalgo-Gonzalez <paty.hidalgo.g@gmail.com>
2017-07-13 23:10:49 -0700

    Partial progress for stochastic wecc.

--- switch_model/wecc/stochastic_PySP/pha_bounds_cfg.py
+++ switch_model/wecc/stochastic_PySP/pha_bounds_cfg.py
@@ -7,9 +7,9 @@
 def pysp_boundsetter_callback(self, scenario_tree, scenario):
     m = scenario._instance 	# see pyomo/pysp/scenariotree/tree_structure.py
  
-    # BuildLocalTD
-    for lz, bld_yr in m.LOCAL_TD_BUILD_YEARS - m.EXISTING_LOCAL_TD_BLD_YRS:
-        m.BuildLocalTD[lz, bld_yr].setub(2 * m.lz_peak_demand_mw[lz, bld_yr])
+    # BuildLocalTD # Paty commented this block because we are not including LocalTD in the modules
+  #  for lz, bld_yr in m.LOCAL_TD_BUILD_YEARS - m.EXISTING_LOCAL_TD_BLD_YRS:
+  #      m.BuildLocalTD[lz, bld_yr].setub(2 * m.lz_peak_demand_mw[lz, bld_yr])
 
     # Estimate an upper bound of system peak demand for limiting project
     # & transmission builds
@@ -21,11 +21,11 @@ def pysp_boundsetter_callback(self, scenario_tree, scenario):
     # BuildProj
     for proj, bld_yr in m.PROJECT_BUILDYEARS - m.EXISTING_PROJ_BUILDYEARS:
         if proj not in m.PROJECTS_CAP_LIMITED:
-            m.BuildProj[proj, bld_yr].setub(5 * system_wide_peak[bld_yr])
+            m.BuildGen[proj, bld_yr].setub(5 * system_wide_peak[bld_yr])
 
     # BuildTrans
     for tx, bld_yr in m.NEW_TRANS_BLD_YRS:
-        m.BuildTrans[tx, bld_yr].setub(5 * system_wide_peak[bld_yr])
+        m.BuildTx[tx, bld_yr].setub(5 * system_wide_peak[bld_yr])
 
 # For some reason runph looks for pysp_boundsetter_callback when run in
 # single-thread mode and ph_boundsetter_callback when called from mpirun with
