723875b0726419757c0f8ea681f5109c15096dd2
Patricia Hidalgo-Gonzalez <paty.hidalgo.g@gmail.com>
2017-10-13 16:08:55 -0700

    [rps by load zone] changed name of output file.

--- switch_model/policies/rps_by_load_zone.py
+++ switch_model/policies/rps_by_load_zone.py
@@ -204,7 +204,7 @@ def post_solve(instance, outdir):
         instance,
         instance.ZONE_PERIODS,
         # instance, instance.LOAD_ZONES, instance.PERIODS,
-        output_file=os.path.join(outdir, "rps_energy_v2.txt"),
+        output_file=os.path.join(outdir, "rps_energy.txt"),
         headings=(
             "LOAD_ZONE",
             "PERIOD",
