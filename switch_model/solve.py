0664c12ca716ca2ab89fb42f2c971cafcafbad26
josiah.johnston <josiah.johnston@gmail.com>
2017-12-08 11:27:55 -0800

    Bugfix for dispatch export columns of annual energy and variable costs. Improve export of transmission builds. Avoid error in solve.py when IPython isn't installed.

--- switch_model/solve.py
+++ switch_model/solve.py
@@ -12,7 +12,10 @@ from switch_model.utilities import create_model, _ArgumentParser, Logging
 from switch_model.upgrade import do_inputs_need_upgrade, upgrade_inputs
 
 #paty's adition for debugging:
-from IPython import embed
+try:
+    from IPython import embed
+except:
+    pass
 
 
 def main(args=None, return_model=False, return_instance=False):
