c53df0757025a5ad30d66488a91a64b32582a9bf
josiah.johnston <josiah.johnston@gmail.com>
2017-12-08 11:38:05 -0800

    Export energy costs per period

--- switch_model/transmission/transport/build.py
+++ switch_model/transmission/transport/build.py
@@ -366,4 +366,3 @@ def post_solve(instance, outdir):
     tx_build_df = pd.DataFrame(normalized_dat)
     tx_build_df.set_index(["TRANSMISSION_LINE", "PERIOD"], inplace=True)
     tx_build_df.to_csv(os.path.join(outdir, "transmission.csv"))
-    
