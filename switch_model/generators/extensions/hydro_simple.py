29a43663a798f897562baca0483780444ae35da6
josiah.johnston <josiah.johnston@gmail.com>
2019-07-25 13:49:12 -0700

    Update simple_hydro to model spillways; a crucial feature of hydro plants used in high-flow conditions where river flow exceeds the capacity of the generator & reservoir. Also minor improvements to documentation and code formatting for improved readability & accuracy.

--- switch_model/generators/extensions/hydro_simple.py
+++ switch_model/generators/extensions/hydro_simple.py
@@ -9,49 +9,55 @@ as a starting point for working with hydro because it's simple, fairly
 reasonable, and easy to work with. This supports dispatchable resevoir-
 based hydro plants. This module is not needed to support run-of-river
 plants; those may be specified as variable renewable plants with exogenous
-energy availability.
+energy availability. This module is generally sufficient for day-to-day
+operations where reservoir volume levels are not a binding constraint, but
+does not track seasonal planning for fluctuations in reservoir volume.
 
-A more full-featured hydro model is available from the Operations, Control
-and Markets laboratory at Pontificia Universidad Católica de Chile.
-Where possible, I have used the same set and variable names to hopefully
-make it easier to merge that into the codebase later. The Chilean branch
-has a hydro model that includes water networks that connect dams via waterways
-and ground infiltration. It should be possible to describe a simple system
-using the advanced framework, but the advanced framework would take longer to
-read and understand. To really take advantage of it, you'll also need more
-data than we usually have available.
+A more full-featured hydro model is available in the hydro_system module from
+the Operations, Control and Markets laboratory at Pontificia Universidad
+Católica de Chile. Where possible, I have used the same set and variable names
+to aid interoperability. The Chilean module has a hydro model that includes
+water networks that connect dams via waterways and ground infiltration. It
+should be possible to describe a simple system using the advanced framework,
+but the advanced framework would take longer to read and understand. To really
+take advantage of it, you'll also need more data than we usually have
+available.
 """
-from __future__ import division
 # ToDo: Refactor this code to move the core components into a
 # switch_model.hydro.core module, the simplest components into
 # switch_model.hydro.simple, and the advanced components into
 # switch_model.hydro.water_network. That should set a good example
 # for other people who want to do other custom handling of hydro.
 
+from __future__ import division
+
 import logging
 import os
 
 from pyomo.environ import *
 
-dependencies = 'switch_model.timescales', 'switch_model.balancing.load_zones',\
-    'switch_model.financials', 'switch_model.energy_sources.properties.properties', \
-    'switch_model.generators.core.build', 'switch_model.generators.core.dispatch'
+dependencies = (
+    'switch_model.timescales',
+    'switch_model.balancing.load_zones',
+    'switch_model.financials',
+    'switch_model.energy_sources.properties.properties',
+    'switch_model.generators.core.build',
+    'switch_model.generators.core.dispatch'
+)
 
 def define_components(mod):
     """
-
-    HYDRO_GENS is the set of dispatchable hydro projects. This is a subet
-    of GENERATION_PROJECTS, and is determined by the inputs file hydro_timeseries.csv.
-    Members of this set can be called either g, or hydro_g.
+    HYDRO_GENS is the set of dispatchable hydro projects. This is a subset of
+    GENERATION_PROJECTS, and is determined by the inputs file
+    hydro_timeseries.csv.
 
     HYDRO_GEN_TS is the set of Hydro projects and timeseries for which
-    minimum and average flow are specified. Members of this set can be
-    abbreviated as (project, timeseries) or (g, ts).
+    minimum and average flow are specified.
 
     HYDRO_GEN_TPS is the set of Hydro projects and available
     dispatch points. This is a filtered version of GEN_TPS that
     only includes hydro projects.
-
+    
     hydro_min_flow_mw[(g, ts) in HYDRO_GEN_TS] is a parameter that
     determines minimum flow levels, specified in units of MW dispatch.
 
@@ -61,15 +67,24 @@ def define_components(mod):
     Enforce_Hydro_Min_Flow[(g, t) in HYDRO_GEN_TPS] is a
     constraint that enforces minimum flow levels for each timepoint.
 
-    Enforce_Hydro_Avg_Flow[(g, ts) in HYDRO_GEN_TS] is a constraint
-    that enforces average flow levels across each timeseries.
+    SpillHydro[(g, t) in HYDRO_GEN_TPS] is an variable describing the amount
+    of potential hydro power sent over the spillway (as opposed to being used
+    to generate electricity). In high-flow or flood conditions, this can
+    mitigate infeasibilities where the incoming flow is greater than the
+    generator's capacity. It is specified in units of MW.
 
+    Enforce_Hydro_Avg_Flow[(g, ts) in HYDRO_NONPUMPED_GEN_TS] is a constraint
+    that enforces average flow levels across each timeseries. It requires the
+    average of dispatched and spilled hydro over the course of a timeseries
+    must equal to the corresponding hydro_avg_flow_mw parameter.
     """
 
     mod.HYDRO_GEN_TS_RAW = Set(
         dimen=2,
-        validate=lambda m, g, ts: (g in m.GENERATION_PROJECTS) & (ts in m.TIMESERIES))
-
+        validate=lambda m, g, ts: (
+            (g in m.GENERATION_PROJECTS) & (ts in m.TIMESERIES)
+        )
+    )
     mod.HYDRO_GENS = Set(
         initialize=lambda m: set(g for (g, ts) in m.HYDRO_GEN_TS_RAW),
         doc="Dispatchable hydro projects")
@@ -119,9 +134,6 @@ def define_components(mod):
     mod.warn_on_extra_HYDRO_GEN_TS = BuildCheck(
         rule=_warn_on_extra_HYDRO_GEN_TS)
 
-    # To do: Add validation check that timeseries data are specified for every
-    # valid timepoint.
-
     mod.hydro_min_flow_mw = Param(
         mod.HYDRO_GEN_TS_RAW,
         within=NonNegativeReals,
@@ -135,18 +147,21 @@ def define_components(mod):
         mod.HYDRO_GEN_TS_RAW,
         within=NonNegativeReals,
         default=0.0)
+    mod.SpillHydro = Var(
+        mod.HYDRO_GEN_TPS,
+        within=NonNegativeReals)
     mod.Enforce_Hydro_Avg_Flow = Constraint(
         mod.HYDRO_GEN_TS,
         rule=lambda m, g, ts: (
-            sum(m.DispatchGen[g, t] for t in m.TPS_IN_TS[ts]) / m.ts_num_tps[ts]
-            == m.hydro_avg_flow_mw[g, ts]))
+            sum(m.DispatchGen[g, t] + m.SpillHydro[g,t]
+                for t in m.TPS_IN_TS[ts]
+            ) == m.hydro_avg_flow_mw[g, ts] * m.ts_num_tps[ts]))
 
     mod.min_data_check('hydro_min_flow_mw', 'hydro_avg_flow_mw')
 
 
 def load_inputs(mod, switch_data, inputs_dir):
     """
-
     Import hydro data. The single file hydro_timeseries.csv needs to contain
     entries for each dispatchable hydro project. The set of hydro projects
     is derived from this file, and this file should cover all time periods
