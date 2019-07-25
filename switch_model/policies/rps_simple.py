90abd1cc31dc0218bcd07b4152db7044f98b5c50
josiah.johnston <josiah.johnston@gmail.com>
2019-07-25 13:35:10 -0700

    Fix long-standing bug that used the wrong parameter name in `load_aug()` (autoselect vs auto_select)

--- switch_model/policies/rps_simple.py
+++ switch_model/policies/rps_simple.py
@@ -144,7 +144,7 @@ def load_inputs(mod, switch_data, inputs_dir):
     )
     switch_data.load_aug(
         filename=os.path.join(inputs_dir, "rps_targets.csv"),
-        autoselect=True,
+        auto_select=True,
         index=mod.RPS_PERIODS,
         param=(mod.rps_target,),
     )
