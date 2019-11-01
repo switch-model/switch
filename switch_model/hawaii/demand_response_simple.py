81967edb704070afb469a137658e85226b4f1667
Matthias Fripp <mfripp@hawaii.edu>
2019-11-01 11:42:46 -1000

    Improve default demand response reserves and share code
    balancing.demand_response_simple now defaults to providing 'spinning' 
    reserves if a reserve module is loaded, otherwise providing no reserves. 
    
    Reserve-registration code has also been moved into a function so it can 
    be shared with demand_response_iterative.

--- switch_model/hawaii/demand_response_simple.py
+++ switch_model/hawaii/demand_response_simple.py
@@ -6,10 +6,11 @@ from switch_model.financials import capital_recovery_factor as crf
 def define_arguments(argparser):
     argparser.add_argument('--demand-response-share', type=float, default=0.30,
         help="Fraction of hourly load that can be shifted to other times of day (default=0.30)")
-    argparser.add_argument('--demand-response-reserve-types', nargs='+', default=['spinning'],
+    argparser.add_argument('--demand-response-reserve-types', nargs='+', default=[],
         help=
             "Type(s) of reserves to provide from demand response (e.g., 'contingency' or 'regulation'). "
-            "Specify 'none' to disable."
+            "Specify 'none' to disable. Default is 'spinning' if an operating reserve module is used, "
+            "otherwise it is 'none'."
     )
 
 def define_components(m):
@@ -47,64 +48,82 @@ def define_components(m):
     # add demand response to the zonal energy balance
     m.Zone_Power_Withdrawals.append('ShiftDemand')
 
+
+    if hasattr(m, 'ZONES_IN_BALANCING_AREA'):
+        # calculate available slack from demand response
+        # (from supply perspective, so "up" means less load)
+        m.DemandResponseSlackUp = Expression(m.BALANCING_AREA_TIMEPOINTS, rule=lambda m, b, t:
+            sum(
+                m.ShiftDemand[z, t] -  m.ShiftDemand[z, t].lb
+                for z in m.ZONES_IN_BALANCING_AREA[b]
+            )
+        )
+        m.DemandResponseSlackDown = Expression(m.BALANCING_AREA_TIMEPOINTS, rule=lambda m, b, tp:
+            sum(
+                # difference between scheduled load and max allowed
+                m.demand_response_max_increase * m.zone_demand_mw[z, tp]
+                - m.ShiftDemand[z, tp]
+                for z in m.ZONES_IN_BALANCING_AREA[b]
+            )
+        )
+    register_demand_response_reserves(m)
+
+def register_demand_response_reserves(m):
+    if m.options.demand_response_reserve_types == []:
+        if hasattr(m, 'Spinning_Reserve_Up_Provisions'):
+            m.options.demand_response_reserve_types == ['spinning']
+        else:
+            m.options.demand_response_reserve_types == ['none']
+
     if [rt.lower() for rt in m.options.demand_response_reserve_types] != ['none']:
         # Register with spinning reserves
-        if hasattr(m, 'Spinning_Reserve_Up_Provisions'):
-            # calculate available slack from demand response
-            # (from supply perspective, so "up" means less load)
-            m.DemandResponseSlackUp = Expression(m.BALANCING_AREA_TIMEPOINTS, rule=lambda m, b, t:
-                sum(
-                    m.ShiftDemand[z, t] -  m.ShiftDemand[z, t].lb
-                    for z in m.ZONES_IN_BALANCING_AREA[b]
-                )
+        if not hasattr(m, 'Spinning_Reserve_Up_Provisions'):
+            raise ValueError(
+                "--demand-response-reserve-types is set to a value other than "
+                "'none' ({}). This requires that a spinning reserve module be "
+                "specified in modules.txt."
+                .format(m.options.demand_response_reserve_types)
             )
-            m.DemandResponseSlackDown = Expression(m.BALANCING_AREA_TIMEPOINTS, rule=lambda m, b, tp:
-                sum(
-                    # difference between scheduled load and max allowed
-                    m.demand_response_max_increase * m.zone_demand_mw[z, tp]
-                    - m.ShiftDemand[z, tp]
-                    for z in m.ZONES_IN_BALANCING_AREA[b]
-                )
+
+        if hasattr(m, 'GEN_SPINNING_RESERVE_TYPES'):
+            # using advanced formulation, index by reserve type, balancing area, timepoint
+            # define variables for each type of reserves to be provided
+            # choose how to allocate the slack between the different reserve products
+            m.DR_SPINNING_RESERVE_TYPES = Set(
+                initialize=m.options.demand_response_reserve_types
             )
-            if hasattr(m, 'GEN_SPINNING_RESERVE_TYPES'):
-                # using advanced formulation, index by reserve type, balancing area, timepoint
-                # define variables for each type of reserves to be provided
-                # choose how to allocate the slack between the different reserve products
-                m.DR_SPINNING_RESERVE_TYPES = Set(
-                    initialize=m.options.demand_response_reserve_types
+            m.DemandResponseSpinningReserveUp = Var(
+                m.DR_SPINNING_RESERVE_TYPES, m.BALANCING_AREA_TIMEPOINTS,
+                within=NonNegativeReals
+            )
+            m.DemandResponseSpinningReserveDown = Var(
+                m.DR_SPINNING_RESERVE_TYPES, m.BALANCING_AREA_TIMEPOINTS,
+                within=NonNegativeReals
+            )
+            # constrain reserve provision within available slack
+            m.Limit_DemandResponseSpinningReserveUp = Constraint(
+                m.BALANCING_AREA_TIMEPOINTS,
+                rule=lambda m, ba, tp:
+                    sum(
+                        m.DemandResponseSpinningReserveUp[rt, ba, tp]
+                        for rt in m.DR_SPINNING_RESERVE_TYPES
+                    ) <= m.DemandResponseSlackUp[ba, tp]
+            )
+            m.Limit_DemandResponseSpinningReserveDown = Constraint(
+                m.BALANCING_AREA_TIMEPOINTS,
+                rule=lambda m, ba, tp:
+                    sum(
+                        m.DemandResponseSpinningReserveDown[rt, ba, tp]
+                        for rt in m.DR_SPINNING_RESERVE_TYPES
+                    ) <= m.DemandResponseSlackDown[ba, tp]
+            )
+            m.Spinning_Reserve_Up_Provisions.append('DemandResponseSpinningReserveUp')
+            m.Spinning_Reserve_Down_Provisions.append('DemandResponseSpinningReserveDown')
+        else:
+            # using older formulation, only one type of spinning reserves, indexed by balancing area, timepoint
+            if m.options.demand_response_reserve_types != ['spinning']:
+                raise ValueError(
+                    'Unable to use reserve types other than "spinning" with simple spinning reserves module.'
                 )
-                m.DemandResponseSpinningReserveUp = Var(
-                    m.DR_SPINNING_RESERVE_TYPES, m.BALANCING_AREA_TIMEPOINTS,
-                    within=NonNegativeReals
-                )
-                m.DemandResponseSpinningReserveDown = Var(
-                    m.DR_SPINNING_RESERVE_TYPES, m.BALANCING_AREA_TIMEPOINTS,
-                    within=NonNegativeReals
-                )
-                # constrain reserve provision within available slack
-                m.Limit_DemandResponseSpinningReserveUp = Constraint(
-                    m.BALANCING_AREA_TIMEPOINTS,
-                    rule=lambda m, ba, tp:
-                        sum(
-                            m.DemandResponseSpinningReserveUp[rt, ba, tp]
-                            for rt in m.DR_SPINNING_RESERVE_TYPES
-                        ) <= m.DemandResponseSlackUp[ba, tp]
-                )
-                m.Limit_DemandResponseSpinningReserveDown = Constraint(
-                    m.BALANCING_AREA_TIMEPOINTS,
-                    rule=lambda m, ba, tp:
-                        sum(
-                            m.DemandResponseSpinningReserveDown[rt, ba, tp]
-                            for rt in m.DR_SPINNING_RESERVE_TYPES
-                        ) <= m.DemandResponseSlackDown[ba, tp]
-                )
-                m.Spinning_Reserve_Up_Provisions.append('DemandResponseSpinningReserveUp')
-                m.Spinning_Reserve_Down_Provisions.append('DemandResponseSpinningReserveDown')
-            else:
-                # using older formulation, only one type of spinning reserves, indexed by balancing area, timepoint
-                if m.options.demand_response_reserve_types != ['spinning']:
-                    raise ValueError(
-                        'Unable to use reserve types other than "spinning" with simple spinning reserves module.'
-                    )
-                m.Spinning_Reserve_Up_Provisions.append('DemandResponseSlackUp')
-                m.Spinning_Reserve_Down_Provisions.append('DemandResponseSlackDown')
+            m.Spinning_Reserve_Up_Provisions.append('DemandResponseSlackUp')
+            m.Spinning_Reserve_Down_Provisions.append('DemandResponseSlackDown')
