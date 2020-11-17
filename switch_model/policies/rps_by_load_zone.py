9dfb196d4a0a003f3ef0bb455b74220747c2eca4
Martin Staadecker <machstg@gmail.com>
2020-11-17 18:15:50 -0500

    Add support for Python 3 add fix indenting

--- switch_model/policies/rps_by_load_zone.py
+++ switch_model/policies/rps_by_load_zone.py
@@ -213,13 +213,13 @@ def post_solve(instance, outdir):
             "TotalGenerationInPeriodGWh",
             "RPSGenFraction",
         ),
-        values=lambda m, (z, p): (
-            z,
-            p,
-            m.RPSFuelEnergy[z, p] / 1000,
-            m.RPSNonFuelEnergy[z, p] / 1000,
-            total_generation_in_load_zone_in_period(m, z, p) / 1000,
-            (m.RPSFuelEnergy[z, p] + m.RPSNonFuelEnergy[z, p])
-            / total_generation_in_load_zone_in_period(m, z, p),
+        values=lambda m, z_p: (
+            z_p[0],
+            z_p[1],
+            m.RPSFuelEnergy[z_p] / 1000,
+            m.RPSNonFuelEnergy[z_p] / 1000,
+            total_generation_in_load_zone_in_period(m, z_p) / 1000,
+            (m.RPSFuelEnergy[z_p] + m.RPSNonFuelEnergy[z_p])
+            / total_generation_in_load_zone_in_period(m, z_p),
         ),
     )
