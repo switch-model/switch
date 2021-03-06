689ef550272eea7f790bfd01ce08a7385b9f9652
Martin Staadecker <machstg@gmail.com>
2021-03-06 15:18:13 -0500

    Add comments and better timing

--- switch_model/wecc/scale.py
+++ switch_model/wecc/scale.py
@@ -1,14 +1,27 @@
 from pyomo.environ import Suffix, TransformationFactory
 import time
 
-def scale(model):
-    model.scaling_factor = Suffix(direction=Suffix.LOCAL)
-    model.scaling_factor[model.Minimize_System_Cost] = 10**-8
+from switch_model.utilities import StepTimer
 
-    print("Transforming starting at {}".format(time.time()))
+"""
+Offers two functions scale() and unscale() to ensure
+constants and variables are within a reasonable range.
+Used when the --wecc-scale flag is used.
+"""
+
+def scale(model):
+    step_timer = StepTimer()
+    print("Scaling variables...")
+
+    model.scaling_factor = Suffix(direction=Suffix.LOCAL)
+    model.scaling_factor[model.Minimize_System_Cost] = 10**-9
+
+
     scaled_model = TransformationFactory('core.scale_model').create_using(model)
     del scaled_model.scaling_factor
-    print("Done at {}".format(time.time()))
+
+    print("Done scaling in {} s".format(step_timer.step_time()))
+
     return scaled_model, model
 
 
