00b701a3a40972a64f19e1786b06e913d65dee9a
Martin Staadecker <machstg@gmail.com>
2021-04-05 23:25:59 -0400

    Add locational marginal pricing

--- switch_model/balancing/load_zones.py
+++ switch_model/balancing/load_zones.py
@@ -112,6 +112,10 @@ def define_components(mod):
         ),
     )
 
+    # Make sure the model has a dual suffix for determining locational marginal pricing
+    if not hasattr(mod, "dual"):
+        mod.dual = Suffix(direction=Suffix.IMPORT)
+
 
 def define_dynamic_components(mod):
     """
@@ -192,7 +196,31 @@ def post_solve(instance, outdir):
     Zone_Power_Injections and Zone_Power_Withdrawals will
     become a column.
 
+    Locational marginal pricing is also included in load_balance.csv
+    and is found from the duals. This value is only available
+    for linear optimization problems.
     """
+
+    def get_output_row(m, z, t):
+        # Add index to row
+        row = [z, m.tp_timestamp[t]]
+
+        # Add locational marginal pricing to row if it's available
+        if not m.has_discrete_variables() and m.Zone_Energy_Balance[z, t] in m.dual:
+            row.append(m.dual[m.Zone_Energy_Balance[z, t]])
+        else:
+            row.append(".")
+
+        # Add contributions to energy balance to the row
+        row.extend(
+            [
+                getattr(m, component)[z, t]
+                for component in (m.Zone_Power_Injections + m.Zone_Power_Withdrawals)
+            ]
+        )
+
+        return row
+
     write_table(
         instance,
         instance.LOAD_ZONES,
@@ -201,14 +229,8 @@ def post_solve(instance, outdir):
         headings=(
             "load_zone",
             "timestamp",
+            "lmp_dollar_per_mw",
         )
         + tuple(instance.Zone_Power_Injections + instance.Zone_Power_Withdrawals),
-        values=lambda m, z, t: (
-            z,
-            m.tp_timestamp[t],
-        )
-        + tuple(
-            getattr(m, component)[z, t]
-            for component in (m.Zone_Power_Injections + m.Zone_Power_Withdrawals)
-        ),
+        values=get_output_row,
     )
