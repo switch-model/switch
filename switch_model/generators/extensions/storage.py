3f489d07e11de075be4c4f9716c84b6621701273
Martin Staadecker <machstg@gmail.com>
2021-04-09 23:22:38 -0400

    Second scaling attempt

--- switch_model/generators/extensions/storage.py
+++ switch_model/generators/extensions/storage.py
@@ -10,6 +10,7 @@ storage, when to charge, energy accounting, etc.
 from pyomo.environ import *
 import os, collections
 from switch_model.financials import capital_recovery_factor as crf
+from switch_model.utilities.scaling import ScaledVariable
 
 dependencies = 'switch_model.timescales', 'switch_model.balancing.load_zones',\
     'switch_model.financials', 'switch_model.energy_sources.properties', \
@@ -147,10 +148,12 @@ def define_components(mod):
                     m.gen_predetermined_storage_energy_mwh[g, bld_yr])
         else:
             return (0, None)
-    mod.BuildStorageEnergy = Var(
+
+    mod.BuildStorageEnergy = ScaledVariable(
         mod.STORAGE_GEN_BLD_YRS,
         within=NonNegativeReals,
-        bounds=bounds_BuildStorageEnergy)
+        bounds=bounds_BuildStorageEnergy,
+        scaling_factor=10 ** -4)
 
     # Some projects are retired before the first study period, so they
     # don't appear in the objective function or any constraints.
--- switch_model/transmission/transport/build.py
+++ switch_model/transmission/transport/build.py
@@ -9,6 +9,7 @@ import os
 from pyomo.environ import *
 from switch_model.financials import capital_recovery_factor as crf
 import pandas as pd
+from switch_model.utilities.scaling import ScaledVariable
 
 dependencies = 'switch_model.timescales', 'switch_model.balancing.load_zones',\
     'switch_model.financials'
@@ -196,7 +197,7 @@ def define_components(mod):
         dimen=2,
         initialize=mod.TRANSMISSION_LINES * mod.PERIODS,
         filter=lambda m, tx, p: m.trans_new_build_allowed[tx])
-    mod.BuildTx = Var(mod.TRANS_BLD_YRS, within=NonNegativeReals)
+    mod.BuildTx = ScaledVariable(mod.TRANS_BLD_YRS, within=NonNegativeReals, scaling_factor=10 ** -5)
     mod.TxCapacityNameplate = Expression(
         mod.TRANSMISSION_LINES, mod.PERIODS,
         rule=lambda m, tx, period: sum(
