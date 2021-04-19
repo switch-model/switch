dbf08a6775d3c91c51282152e9031ad23c348079
Martin Staadecker <machstg@gmail.com>
2021-04-19 00:22:26 -0400

    unscale DispatchGen since bounds were too low

--- switch_model/generators/core/dispatch.py
+++ switch_model/generators/core/dispatch.py
@@ -234,7 +234,6 @@ def define_components(mod):
         rule=lambda m, g, t: m.GenCapacity[g, m.tp_period[t]])
     mod.DispatchGen = ScaledVariable(
         mod.GEN_TPS,
-        scaling_factor=10 ** -3,
         within=NonNegativeReals)
     mod.DispatchGenByFuel = Var(mod.GEN_TP_FUELS, within=NonNegativeReals)
     mod.DispatchGenByFuel_Constraint = Constraint(
