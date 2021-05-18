09f622c90c5c17c8d7c2c2143f0bfee91fdc8044
Martin Staadecker <machstg@gmail.com>
2021-05-18 11:58:47 -0700

    Remove fuel supply curve inf rows

--- switch_model/wecc/get_inputs.py
+++ switch_model/wecc/get_inputs.py
@@ -135,6 +135,8 @@ def connect(schema="switch"):
     return conn
 
 
+SKIP_VAR_CAPACITY_FACTORS = True
+
 def create_csvs():
     timer = StepTimer()
 
@@ -645,19 +647,25 @@ def create_csvs():
     # Pyomo will raise an error if a capacity factor is defined for a project on a timepoint when it is no longer operational (i.e. Canela 1 was built on 2007 and has a 30 year max age, so for tp's ocurring later than 2037, its capacity factor must not be written in the table).
 
     # variable_capacity_factors.csv
-    write_csv_from_query(
-        db_cursor,
-        "variable_capacity_factors",
-        ["GENERATION_PROJECT", "timepoint", "gen_max_capacity_factor"],
-        f"""
-            select generation_plant_id, t.raw_timepoint_id, capacity_factor
-            FROM variable_capacity_factors_exist_and_candidate_gen v
-                JOIN generation_plant_scenario_member USING(generation_plant_id)
-                JOIN sampled_timepoint as t ON(t.raw_timepoint_id = v.raw_timepoint_id)
-            WHERE generation_plant_scenario_id = {generation_plant_scenario_id}
-                AND t.time_sample_id={time_sample_id};
-            """,
-    )
+    if not SKIP_VAR_CAPACITY_FACTORS:
+        write_csv_from_query(
+            db_cursor,
+            "variable_capacity_factors",
+            ["GENERATION_PROJECT", "timepoint", "gen_max_capacity_factor"],
+            f"""
+                select 
+                    generation_plant_id, 
+                    t.raw_timepoint_id, 
+                    -- we round down when the capacity factor is less than 1e-5 to avoid numerical issues and simplify our model
+                    -- performance wise this doesn't have any significant impact
+                    case when abs(capacity_factor) < 0.00001 then 0 else capacity_factor end
+                FROM variable_capacity_factors_exist_and_candidate_gen v
+                    JOIN generation_plant_scenario_member USING(generation_plant_id)
+                    JOIN sampled_timepoint as t ON(t.raw_timepoint_id = v.raw_timepoint_id)
+                WHERE generation_plant_scenario_id = {generation_plant_scenario_id}
+                    AND t.time_sample_id={time_sample_id};
+                """,
+        )
 
     ########################################################
     # HYDROPOWER
@@ -776,7 +784,13 @@ def create_csvs():
                             else max_avail_at_cost::varchar end) as max_avail_at_cost
                 from switch.fuel_supply_curves
                 join switch.period on(year>=start_year)
-                where year=FLOOR(period.start_year + length_yrs/2-1)
+                where year=FLOOR(period.start_year + length_yrs/2-1) 
+                -- we filter out extremly large unit_costs that are only used to indicate that we should never
+                -- buy at this price point. This is to simplify the model and improve its numerical properties.
+                and not (
+                    unit_cost > 1e9
+                    and max_avail_at_cost is null
+                )
                 and study_timeframe_id = {study_timeframe_id} 
                 and supply_curves_scenario_id = {supply_curves_scenario_id};
                             """,
