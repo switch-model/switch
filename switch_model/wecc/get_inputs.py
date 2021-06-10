fd375e430b1ea1b40f2f90e635707ee8ac4ff171
Martin Staadecker <machstg@gmail.com>
2021-06-09 17:10:02 -0700

    Move 2020 prebuilds to 2019.

--- switch_model/wecc/get_inputs.py
+++ switch_model/wecc/get_inputs.py
@@ -135,7 +135,7 @@ def connect(schema="switch"):
     return conn
 
 
-def create_csvs():
+def main():
     timer = StepTimer()
 
     # Create command line tool, just provides help information
@@ -149,11 +149,21 @@ def create_csvs():
     parser.add_argument("--skip-cf", default=False, action='store_true',
                         help="Skip creation variable_capacity_factors.csv. Useful when debugging and one doesn't"
                              "want to wait for the command.")
+    parser.add_argument("--post-only", default=False, action='store_true',
+                        help="Only run the post solve functions (don't query db)")
     args = parser.parse_args()  # Makes switch get_inputs --help works
 
     # Load values from config.yaml
     full_config = load_config()
     switch_to_input_dir(full_config)
+
+    if not args.post_only:
+        query_db(full_config, skip_cf=args.skip_cf)
+    post_process()
+    print(f"\nScript took {timer.step_time_as_str()} seconds to build input tables.")
+
+
+def query_db(full_config, skip_cf):
     config = full_config["get_inputs"]
     scenario_id = config["scenario_id"]
 
@@ -647,7 +657,7 @@ def create_csvs():
     # Pyomo will raise an error if a capacity factor is defined for a project on a timepoint when it is no longer operational (i.e. Canela 1 was built on 2007 and has a 30 year max age, so for tp's ocurring later than 2037, its capacity factor must not be written in the table).
 
     # variable_capacity_factors.csv
-    if not args.skip_cf:
+    if not skip_cf:
         write_csv_from_query(
             db_cursor,
             "variable_capacity_factors",
@@ -908,8 +918,6 @@ def create_csvs():
         planning_reserves(db_cursor, time_sample_id, hydro_simple_scenario_id)
     create_modules_txt()
 
-    print(f"\nScript took {timer.step_time_as_str()} seconds to build input tables.")
-
 
 def ca_policies(db_cursor, ca_policies_scenario_id, study_timeframe_id):
     if ca_policies_scenario_id is None:
@@ -958,7 +966,6 @@ def ca_policies(db_cursor, ca_policies_scenario_id, study_timeframe_id):
     )
 
     modules.append('switch_model.policies.CA_policies')
-
 
 def planning_reserves(db_cursor, time_sample_id, hydro_simple_scenario_id):
     # reserve_capacity_value.csv specifies the capacity factors that should be used when calculating
@@ -1017,6 +1024,7 @@ def planning_reserves(db_cursor, time_sample_id, hydro_simple_scenario_id):
 
     modules.append("switch_model.balancing.planning_reserves")
 
+
 def create_modules_txt():
     print("modules.txt...")
     with open("modules.txt", "w") as f:
@@ -1025,13 +1033,45 @@ def create_modules_txt():
 
 
 def post_process():
-    # No post-process at the moment
-    pass
+    fix_prebuild_conflict_bug()
 
 
-def main():
-    create_csvs()
-    post_process()
+def fix_prebuild_conflict_bug():
+    """
+    This post-processing step is necessary to pass the no_predetermined_bld_yr_vs_period_conflict BuildCheck.
+    Basically we are moving all the 2020 predetermined build years to 2019 to avoid a conflict with the 2020 period.
+    See generators.core.build.py for details.
+    """
+    # Read two files that need modification
+    gen_build_costs = pd.read_csv("gen_build_costs.csv", index_col=False)
+    gen_build_predetermined = pd.read_csv("gen_build_predetermined.csv", index_col=False)
+    # Save their size
+    rows_prior = gen_build_costs.size, gen_build_predetermined.size
+    # Save columns of gen_build_costs
+    gen_build_costs_col = gen_build_costs.columns
+    # Merge to know which rows are prebuild
+    gen_build_costs = gen_build_costs.merge(
+        gen_build_predetermined,
+        on=["GENERATION_PROJECT", "build_year"],
+        how='left'
+    )
+
+    # If row is prebuild and in 2020, replace it with 2019
+    gen_build_costs.loc[
+        (~gen_build_costs["gen_predetermined_cap"].isna()) & (gen_build_costs["build_year"] == 2020),
+        "build_year"] = 2019
+    # If row is in 2020 replace it with 2019
+    gen_build_predetermined.loc[gen_build_predetermined["build_year"] == 2020, "build_year"] = 2019
+    # Go back to original column set
+    gen_build_costs = gen_build_costs[gen_build_costs_col]
+
+    # Ensure the size is still the same
+    rows_post = gen_build_costs.size, gen_build_predetermined.size
+    assert rows_post == rows_prior
+
+    # Write the files back out
+    gen_build_costs.to_csv("gen_build_costs.csv", index=False)
+    gen_build_predetermined.to_csv("gen_build_predetermined.csv", index=False)
 
 
 if __name__ == "__main__":
