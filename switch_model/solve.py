fb7882b77f328f19d59b19bc4d27c64808f3aa3a
Martin Staadecker <machstg@gmail.com>
2021-06-16 19:30:59 -0700

    Fix deprecation warning

--- switch_model/solve.py
+++ switch_model/solve.py
@@ -776,7 +776,7 @@ def solve(model):
             os.makedirs(model.options.tempdir)
 
         # from https://pyomo.readthedocs.io/en/stable/working_models.html#changing-the-temporary-directory
-        from pyutilib.services import TempfileManager
+        from pyomo.common.tempfiles import TempfileManager
         TempfileManager.tempdir = model.options.tempdir
 
     # Cleanup memory before entering solver to use up as little memory as possible.
