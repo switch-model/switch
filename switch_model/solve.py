c355882ad27657b8740285a3aa74d80571a47eb3
Martin Staadecker <machstg@gmail.com>
2021-07-15 12:10:06 -0700

    Make crossover the default and add --recommended-fast

--- switch_model/solve.py
+++ switch_model/solve.py
@@ -780,12 +780,12 @@ def add_recommended_args(argparser):
     )
 
     argparser.add_argument(
-        "--recommended-robust",
+        "--recommended-fast",
         default=False,
         action="store_true",
-        help="Equivalent to --recommended however enables crossover during solving. Crossover is useful"
-        " if the solution found by the barrier method is suboptimal. If you find that the solver returns"
-        " a suboptimal solution use this flag.",
+        help="Equivalent to --recommended however disables crossover during solving. This reduces"
+        " the solve time greatly however may result in less accurate values and may fail to find an optimal"
+        " solution. If you find that the solver returns a suboptimal solution use --recommended.",
     )
 
     argparser.add_argument(
@@ -802,7 +802,7 @@ def parse_recommended_args(args):
     options = argparser.parse_known_args(args)[0]
 
     flags_used = (
-        options.recommended + options.recommended_robust + options.recommended_debug
+        options.recommended + options.recommended_fast + options.recommended_debug
     )
     if flags_used > 1:
         raise Exception(
@@ -822,9 +822,9 @@ def parse_recommended_args(args):
         "--debug",
         "--graph",
     ] + args
-    solver_options_string = "BarHomogeneous=1 FeasibilityTol=1e-5"
-    if not options.recommended_robust:
-        solver_options_string += " crossover=0 method=2"
+    solver_options_string = "BarHomogeneous=1 FeasibilityTol=1e-5 method=2"
+    if options.recommended_fast:
+        solver_options_string += " crossover=0"
     args = ["--solver-options-string", solver_options_string] + args
     if options.recommended_debug:
         args = ["--keepfiles", "--tempdir", "temp", "--symbolic-solver-labels"] + args
@@ -1039,7 +1039,7 @@ def solve(model):
             and solver_status == SolverStatus.warning
         ):
             print(
-                "If you used --recommended, you might want to try --recommended-robust."
+                "If you used --recommended-fast, you might want to try using just --recommended."
             )
 
         if query_yes_no("Do you want to abort and exit?", default=None):
