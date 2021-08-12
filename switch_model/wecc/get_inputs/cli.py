64b7be53afbe5f123a4f1806c572298aaa92f208
pesap <pesapsanchez@gmail.com>
2021-08-12 14:29:22 -0700

    Change --post-process to post-process-only

--- switch_model/wecc/get_inputs/cli.py
+++ switch_model/wecc/get_inputs/cli.py
@@ -25,7 +25,7 @@ def main():
     parser.add_argument("--skip-cf", default=False, action='store_true',
                         help="Skip creation variable_capacity_factors.csv. Useful when debugging and one doesn't"
                              "want to wait for the command.")
-    parser.add_argument("--post-process", default=False, action='store_true', help="Run only post process steps.")
+    parser.add_argument("--post-process-only", default=False, action='store_true', help="Run only post process steps.")
     parser.add_argument("--post-process-step", default=None, help="Run only this post process step.")
     parser.add_argument("--overwrite", default=False, action='store_true',
                         help="Overwrite previous input files without prompting to confirm.")
@@ -35,42 +35,36 @@ def main():
     full_config = load_config()
     switch_to_input_dir(full_config, overwrite=args.overwrite)
 
-    if args.post_process is None:
+    if not args.post_process_only is None:
         query_db(full_config, skip_cf=args.skip_cf)
 
-    print("Post-processing...")
+    print("\nRunning post processing...")
 
+    # Get location of post process scripts
     post_process_path = '.'.join(__name__.split('.')[:-1]) + ".post_process_steps"
 
     def run_post_process(module):
+        """ Run a function from a given module """
+
+        # This uses python module syntax with a dot. Example: import foo.bar.test
         mod = importlib.import_module(f".{module}", post_process_path)
+
         post_process = getattr(mod, "post_process")
 
-        # Configuration specific for the post_process
+        # Get specific configuration for the post process if specified
         post_config = full_config.get(module, None)
 
         # Run post process
         post_process(full_config, post_config)
 
-    # Get additional post-process from the config file
+    # Run all post process specified, otherwise run single one
     if args.post_process_step is None:
         for module in full_config["post_process"]:
             run_post_process(module)
-        # run_post_process(full_config, step_name=args.post_process)
     else:
         run_post_process(getattr(args, "post_process_step"))
-        # breakpoint()
-            # post_process_path = '.'.join(__name__.split('.')[:-1]) + ".post_process_steps"
-            # mod = importlib.import_module(f".{module}", post_process_path)
-            # post_process = getattr(mod, "post_process")
 
-            # # Configuration specific for the post_process 
-            # post_config = full_config.get(module, None)
-
-            # # Run post process
-            # post_process(full_config, post_config)
-
-    print(f"\nScript took {timer.step_time_as_str()} seconds to build input tables.")
+    print(f"\nScript took {timer.step_time_as_str()} seconds.")
 
 
 def switch_to_input_dir(config, overwrite):
