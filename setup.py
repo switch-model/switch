808c1cfc8e543a13b80f7648fb834458b25f0413
Martin Staadecker <machstg@gmail.com>
2021-09-04 15:08:41 -0400

    Remove outdated patch

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     ],
     python_requires='>=3.7',
     install_requires=[
-        "Pyomo>=6.0",  # We need a version that works with glpk 4.60+
+        "Pyomo>=6.1",  # 6.1 Has all the bug fixes we need
         "pint",  # needed by Pyomo when we run our tests, but not included
         "testfixtures",  # used for standard tests
         "pandas",  # used for input upgrades and testing that functionality
