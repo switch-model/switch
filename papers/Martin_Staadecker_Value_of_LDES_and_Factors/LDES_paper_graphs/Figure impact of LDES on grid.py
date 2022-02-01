3919b354103dc9961552ec425c35e949f4269e21
Martin Staadecker <machstg@gmail.com>
2022-02-01 11:28:18 -0500

    Create LMP figure

--- papers/Martin_Staadecker_Value_of_LDES_and_Factors/LDES_paper_graphs/Figure impact of LDES on grid.py
+++ papers/Martin_Staadecker_Value_of_LDES_and_Factors/LDES_paper_graphs/Figure impact of LDES on grid.py
@@ -6,31 +6,14 @@ import pandas as pd
 from matplotlib.ticker import PercentFormatter
 
 from papers.Martin_Staadecker_Value_of_LDES_and_Factors.LDES_paper_graphs.util import (
-    get_scenario,
     set_style,
+    get_set_e_scenarios,
 )
 from switch_model.tools.graph.main import GraphTools
 
 
 # Prepare graph tools
-tools = GraphTools(
-    scenarios=[
-        get_scenario("1342", name=1.94),
-        get_scenario("M7", name=2),
-        get_scenario("M10", name=2.5),
-        get_scenario("M9", name=3),
-        get_scenario("M6", name=4),
-        get_scenario("M5", name=8),
-        get_scenario("M11", name=12),
-        get_scenario("M4", name=16),
-        get_scenario("M14", name=18),
-        get_scenario("M13", name=20),
-        get_scenario("M8", name=24),
-        get_scenario("M3", name=32),
-        get_scenario("M12", name=48),
-        get_scenario("M2", name=64),
-    ]
-)
+tools = GraphTools(scenarios=get_set_e_scenarios())
 tools.pre_graphing(multi_scenario=True)
 
 # %% CREATE FIGURE
@@ -43,76 +26,6 @@ ax2 = fig.add_subplot(2, 2, 2)
 ax3 = fig.add_subplot(2, 2, 3)
 ax4 = fig.add_subplot(2, 2, 4)
 
-# %% Daily LMP
-
-ax = ax3
-ax.clear()
-time_mapping = {
-    0: "Midnight",
-    4: "4am",
-    8: "8am",
-    12: "Noon",
-    16: "4pm",
-    20: "8pm"
-}
-daily_lmp = tools.get_dataframe(
-    "load_balance.csv",
-    usecols=[
-        "timestamp",
-        "normalized_energy_balance_duals_dollar_per_mwh",
-        "scenario_name",
-    ],
-).rename(columns={"normalized_energy_balance_duals_dollar_per_mwh": "value"})
-daily_lmp = tools.transform.timestamp(daily_lmp)
-daily_lmp = daily_lmp.groupby(["scenario_name", "hour"], as_index=False)["value"].mean()
-daily_lmp = daily_lmp.pivot(index="scenario_name", columns="hour", values="value")
-daily_lmp *= 0.1  # Convert from $/MWh to cents/kWh
-daily_lmp = daily_lmp.rename(time_mapping, axis=1).rename_axis("Time of day (PST)", axis=1)
-daily_lmp.plot(
-    ax=ax,
-    xlabel="WECC-wide storage capacity (TWh)",
-    marker=".",
-    ylabel=u"Estimated LMP (\xa2/kWh)",
-    # cmap="tab10"
-)
-ax.set_title("C. Average estimated LMP by time of day")
-# %% YEARLY LMP
-ax = ax4
-ax.clear()
-scenarios_for_yearly_lmp = [5, 7, 8, 11, 12]
-
-cap = tools.get_dataframe(
-    "load_balance.csv",
-    usecols=[
-        "timestamp",
-        "normalized_energy_balance_duals_dollar_per_mwh",
-        "scenario_name",
-    ],
-).rename(columns={"normalized_energy_balance_duals_dollar_per_mwh": "value"})
-cap = cap.groupby(["scenario_name", "timestamp"], as_index=False).mean()
-cap = tools.transform.timestamp(cap)
-cap = cap.set_index("datetime")
-cap["month"] = cap.index.month
-cap = cap.groupby(["scenario_name", "month"]).value.mean()
-cap = cap.unstack("month").rename_axis("Storage Capacity (TWh)", axis=1)
-# Convert from $/MWh to cents/kWh
-cap *= 0.1
-cap = cap[scenarios_for_yearly_lmp]
-cap = cap.rename({
-    5: "May",
-    7: "July",
-    8: "August",
-    11: "November",
-    12: "December",
-}, axis=1)
-cap.plot(
-    ax=ax,
-    xlabel="WECC-wide storage capacity (TWh)",
-    marker=".",
-    ylabel=u"Monthly-average estimated LMP (\xa2/kWh)",
-    # cmap="tab10"
-)
-ax.set_title("D. Average estimated LMP during key months")
 # %% IMPACT ON TX AND GEN
 
 ax = ax2
@@ -149,7 +62,9 @@ buildout = tools.transform.gen_type(buildout)
 # Filter out storage since it's not considered generation
 buildout = buildout[buildout["gen_type"] != "Storage"]
 # Sum across the entire scenario
-buildout = buildout.groupby("scenario_name")["BuildGen"].sum().rename("Built Generation")
+buildout = (
+    buildout.groupby("scenario_name")["BuildGen"].sum().rename("Built Generation")
+)
 
 cap = tools.get_dataframe(
     "gen_cap.csv",
@@ -166,7 +81,7 @@ df = pd.concat([tx, buildout, cap], axis=1)
 # Convert to percent against baseline
 df = (df / df.iloc[0] - 1) * 100
 
-dotted_tx = df.loc[[1.94,3,20,64], ["Built Transmission"]]
+dotted_tx = df.loc[[1.94, 3, 20, 64], ["Built Transmission"]]
 
 # Plot
 colors = tools.get_colors()
@@ -200,9 +115,15 @@ curtailment = curtailment[curtailment["is_renewable"]]
 # Add the gen_type column
 curtailment = tools.transform.gen_type(curtailment)
 # Convert to GW
-curtailment["value"] = curtailment["Curtailment_MW"] * curtailment["tp_weight_in_year_hrs"] / 1000
-curtailment = curtailment.groupby(["scenario_name", "gen_type"], as_index=False).value.sum()
-curtailment = curtailment.pivot(index="scenario_name", columns="gen_type", values="value")
+curtailment["value"] = (
+    curtailment["Curtailment_MW"] * curtailment["tp_weight_in_year_hrs"] / 1000
+)
+curtailment = curtailment.groupby(
+    ["scenario_name", "gen_type"], as_index=False
+).value.sum()
+curtailment = curtailment.pivot(
+    index="scenario_name", columns="gen_type", values="value"
+)
 curtailment /= 1000
 curtailment = curtailment.rename_axis("Technology", axis=1)
 curtailment.plot(ax=ax, color=tools.get_colors(), marker=".")
@@ -211,7 +132,9 @@ ax.set_xlabel("WECC-wide storage capacity (TWh)")
 ax.set_title("A. Impact of LDES on curtailment")
 ax.tick_params(top=False, bottom=False, right=False, left=False)
 # %%
-plt.subplots_adjust(hspace=0.2, wspace=0.25, left=0.07, right=0.97, top=0.95, bottom=0.07)
+plt.subplots_adjust(
+    hspace=0.2, wspace=0.25, left=0.07, right=0.97, top=0.95, bottom=0.07
+)
 
 # %% CALCULATIONS
 cap_total = cap["Solar"] + cap["Wind"]
@@ -224,8 +147,8 @@ cap_total
 1 - cap / cap.iloc[0]
 cap - cap.iloc[0]
 # %% change from 20 to 64
-1 - cap / cap.loc[20] # wind decrease %
-cap / cap.loc[20] - 1 # solar increase %
+1 - cap / cap.loc[20]  # wind decrease %
+cap / cap.loc[20] - 1  # solar increase %
 (cap - cap.loc[20]) / 1000
 # %% transmission
 tx / tx.iloc[0] * 100
@@ -237,4 +160,7 @@ df.max()
 # %% average drop in daily duals
 df = daily_lmp.mean(axis=1)
 df / df.iloc[0] - 1
-daily_lmp / daily_lmp.iloc[0] - 1
+# daily_lmp / daily_lmp.iloc[0] - 1
+# %% night time drop
+df = daily_lmp[["Midnight", "8pm", "4am"]].mean(axis=1)
+(1 - df.loc[3] / df.iloc[0]) * 100 / ((3 - 1.94) * 10)
