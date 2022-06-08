92181e16c0f847efa4b4136e9ccd10ae50d037dd
pesap <pesap@Tlaloc>
2022-06-08 11:53:26 -0700

    Updating post process with new format

--- switch_model/wecc/get_inputs/post_process_steps/only_california.py
+++ switch_model/wecc/get_inputs/post_process_steps/only_california.py
@@ -5,7 +5,7 @@ from switch_model.tools.drop import main as drop
 
 
 @post_process_step(msg="Dropping all the zones outside of California")
-def main(_):
+def post_process(_):
     df = pd.read_csv("load_zones.csv", index_col=False)
     df = df[df["LOAD_ZONE"].str.startswith("CA_")]
     df.to_csv("load_zones.csv", index=False)
