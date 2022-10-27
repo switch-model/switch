70ad996ff2f53090fd3f28082006b9495cf732e9
Matthias Fripp <mfripp@hawaii.edu>
2022-10-27 15:59:30 -0700

    Split reporting of generator additions and capacity, and include storage values
    This commit changes gen_cap.csv to include both power and energy (storage)
    capacity in place in each period. gen_cap.csv also shows the annual capital
    recovery and fixed O&M required for each project. This commit also adds a
    gen_build.csv file that shows the amount of power and energy capacity _added_
    in each period and the total capital outlay needed when those are built.
    (Switch uses capital outlay to calculate capital recovery requirements, but
    doesn't outlay the capital outlay directly. However, capital outlay is of
    interest to some users.)

--- switch_model/generators/extensions/storage.py
+++ switch_model/generators/extensions/storage.py
@@ -344,50 +344,12 @@ def load_inputs(mod, switch_data, inputs_dir):
 
 def post_solve(instance, outdir):
     """
-    Export storage build information to storage_builds.csv, and storage
-    dispatch info to storage_dispatch.csv
+    Export storage dispatch info to storage_dispatch.csv
+
+    Note that construction information is reported by the generators.core.build
+    module, so is not reported here.
     """
     import switch_model.reporting as reporting
-
-    reporting.write_table(
-        instance,
-        instance.STORAGE_GEN_BLD_YRS,
-        output_file=os.path.join(outdir, "storage_builds.csv"),
-        headings=(
-            "generation_project",
-            "period",
-            "load_zone",
-            "IncrementalPowerCapacityMW",
-            "IncrementalEnergyCapacityMWh",
-        ),
-        values=lambda m, g, bld_yr: (
-            g,
-            bld_yr,
-            m.gen_load_zone[g],
-            m.BuildGen[g, bld_yr],
-            m.BuildStorageEnergy[g, bld_yr],
-        ),
-    )
- 
-    reporting.write_table(
-        instance, 
-        instance.STORAGE_GEN_PERIODS,
-        output_file=os.path.join(outdir, "storage_capacity.csv"),
-        headings=(
-            "generation_project", 
-            "period", 
-            "load_zone",
-            "OnlinePowerCapacityMW", 
-            "OnlineEnergyCapacityMWh" 
-        ),
-        values=lambda m, g, p: (
-            g, 
-            p, 
-            m.gen_load_zone[g],
-            m.GenCapacity[g, p], 
-            m.StorageEnergyCapacity[g, p]
-        )
-    )
 
     reporting.write_table(
         instance,
